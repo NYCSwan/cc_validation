bb01051aa76f0155da67c060d81b7d16c37ae2f1
Megan Swanby <megan.swanby@gmail.com>
2024-01-13 10:34:25 -0500

    server: codegen ts, add validation mutation

--- graphql.config.js
+++ graphql.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   projects: {
     app: {
-      schema: ["schema.graphql"],
+      schema: ['schema.graphql'],
       extensions: {
         endpoints: {
           default: {
-            url: "http://localhost:4000/graphql",
+            url: 'http://localhost:4000/graphql',
           },
         },
       },
--- server/index.ts
+++ server/index.ts
@@ -1,34 +1,50 @@
-import { ApolloServer } from '@apollo/server';
+import { ApolloServer, BaseContext } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { readFileSync } from 'fs';
+import { CreditCard, Resolvers } from './resolvers-types.generated';
 
-const typeDefs = `
-type Query {
-  creditCard:  () => CreditCard
-}
-  type Mutation {
-    validateCreditCard(cardNumber: String!): Boolean
-  }
-  type CreditCard {
-    cardNumber: String
-  }
-`;
+const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
 
 const creditCard = {
   number: '1242345',
 };
-const resolvers = {
+export interface CreditCardValidationContext extends BaseContext {
+  Query: {
+    creditCard: CreditCard;
+  };
+  Mutation: {
+    validateCreditCard: (creditCardNumber: string) => boolean;
+  };
+}
+// fix type
+const resolvers: Resolvers<any> = {
   Query: {
     creditCard: () => creditCard,
   },
   Mutation: {
-    validateCreditCard: (parent, args) => {
-      console.log(args.cardNumber);
-      return true;
+    validateCreditCard: async (_, args) => {
+      const { creditCardNumber } = args;
+      if (!creditCardNumber) {
+        throw new Error('No card number provided');
+      }
+      const checksum = (c: number): number =>
+        c < 10 ? c : checksum(Math.trunc(c / 10) + (c % 10));
+
+      return (
+        creditCardNumber
+          .split('')
+          .reverse()
+          .map(Number)
+          .map((c, i) => (i % 2 !== 0 ? checksum(c * 2) : c))
+          .reduce((acc, digit) => acc + digit) %
+          10 ===
+        0
+      );
     },
   },
 };
 
-const server = new ApolloServer({
+const server = new ApolloServer<BaseContext>({
   typeDefs,
   resolvers,
 });
